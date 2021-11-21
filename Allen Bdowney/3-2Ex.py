6261ae7e2920e015c1ef1319c939daa34d1b275f
Muhammod Abul Bashar <45920385+Bashar12345@users.noreply.github.com>
2021-11-21 17:08:26 +0600

    solved

--- Allen Bdowney/3-2Ex.py
+++ Allen Bdowney/3-2Ex.py
@@ -1,20 +1,21 @@
 #i
 def do_twice(f,val):
     f(val)
-    #f()
-
-def print_spam():
-    print("spam")
-
-#do_twice(print_spam)
+    f(val)
 
 #ii
-do_twice(print_spam(),2)
-
-#iii
 def print_twice(p):
     print(p)
     print(p)
 
+#iii
+def do_four(f,val):
+   do_twice(f,val)
+   do_twice(f,val)
+
+
+do_twice(print,'spam')
+
 #iv
-do_twice(print_twice('spam'),'argument_value')
+do_four(print,'spam')
+
