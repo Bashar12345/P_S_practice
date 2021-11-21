f03aab7a5d0f451fa85f97cb67b698f9eccadb3c
Muhammod Abul Bashar <45920385+Bashar12345@users.noreply.github.com>
2021-11-21 16:12:15 +0600

    init

--- Allen Bdowney/3-2Ex.py
+++ Allen Bdowney/3-2Ex.py
@@ -1,3 +1,20 @@
-def do_twice(f):
-    f()
-    f()
+#i
+def do_twice(f,val):
+    f(val)
+    #f()
+
+def print_spam():
+    print("spam")
+
+#do_twice(print_spam)
+
+#ii
+do_twice(print_spam(),2)
+
+#iii
+def print_twice(p):
+    print(p)
+    print(p)
+
+#iv
+do_twice(print_twice('spam'),'argument_value')
