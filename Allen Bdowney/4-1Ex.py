698b2e6b59ae90d603efcacfae0e82b0c2a8e03b
Muhammod Abul Bashar <45920385+Bashar12345@users.noreply.github.com>
2021-11-25 11:46:21 +0600

    solved

--- Allen Bdowney/4-1Ex.py
+++ Allen Bdowney/4-1Ex.py
@@ -50,7 +50,22 @@ def polyline(t,n,length,angle):
         t.fd(length)
         t.lt(angle)
 
+def arc(t, r, angle):
+    """Draws an arc with the given radius and angle.
+    t: Turtle
+    r: radius
+    angle: angle subtended by the arc, in degrees
+    """
+    arc_length = 2 * math.pi * r * abs(angle) / 360
+    n = int(arc_length / 4) + 3
+    step_length = arc_length / n
+    step_angle = float(angle) / n
 
+    # making a slight left turn before starting reduces
+    # the error caused by the linear approximation of the arc
+    t.lt(step_angle/2)
+    polyline(t, n, step_length, step_angle)
+    t.rt(step_angle/2)
 #polygon(t,7,70)
 #circle(t,90)
 arc(t,7,7)
---
+++ Allen Bdowney/4-2Ex.py
@@ -0,0 +1,95 @@
+
+import math
+import turtle
+
+
+def polygon(t,n,length):
+    angle=360/n
+    for i in range(n):
+        t.fd(length)
+        t.lt(angle)
+
+#50-sided polygon
+def circle(t,r):
+    poridhi=2*3.1416*r
+    n=50
+    n=int(poridhi/3)+1
+    length=poridhi/n
+    polygon(t,n,length)
+
+def arc(t,r,angle):
+    arc_length=2*3.1416*r*angle/360
+    n=int(arc_length/3)+1
+    step_length=arc_length/n
+    step_angle=angle/n
+    for i in range(n):
+        t.fd(step_length)
+        t.lt(step_angle)
+def polyline(t,n,length,angle):
+    for i in range(n):
+        t.fd(length)
+        t.lt(angle)
+
+def arc(t, r, angle):
+    """Draws an arc with the given radius and angle.
+    t: Turtle
+    r: radius
+    angle: angle subtended by the arc, in degrees
+    """
+    arc_length = 2 * math.pi * r * abs(angle) / 360
+    n = int(arc_length / 4) + 3
+    step_length = arc_length / n
+    step_angle = float(angle) / n
+
+    # making a slight left turn before starting reduces
+    # the error caused by the linear approximation of the arc
+    t.lt(step_angle/2)
+    polyline(t, n, step_length, step_angle)
+    t.rt(step_angle/2)
+
+def petal(t, r, angle):
+    """Draws a petal using two arcs.
+    t: Turtle
+    r: radius of the arcs
+    angle: angle (degrees) that subtends the arcs
+    """
+    for i in range(2):
+        arc(t, r, angle)
+        t.lt(180-angle)
+
+
+def flower(t, n, r, angle):
+    """Draws a flower with n petals.
+    t: Turtle
+    n: number of petals
+    r: radius of the arcs
+    angle: angle (degrees) that subtends the arcs
+    """
+    for i in range(n):
+        petal(t, r, angle)
+        t.lt(360.0/n)
+
+
+def move(t, length):
+    """Move Turtle (t) forward (length) units without leaving a trail.
+    Leaves the pen down.
+    """
+    t.pu()
+    t.fd(length)
+    t.pd()
+
+
+bob = turtle.Turtle()
+
+# draw a sequence of three flowers, as shown in the book.
+move(bob, -100)
+flower(bob, 7, 60.0, 60.0)
+
+move(bob, 100)
+flower(bob, 10, 40.0, 80.0)
+
+move(bob, 100)
+flower(bob, 20, 140.0, 20.0)
+
+bob.hideturtle()
+turtle.mainloop()
