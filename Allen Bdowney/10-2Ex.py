4362890873a2ed2f1c8f8e19c8b01dfa096566cf
Muhammod Abul Bashar <45920385+Bashar12345@users.noreply.github.com>
2022-01-11 10:30:53 +0600

    init

--- Allen Bdowney/10-2Ex.py
+++ Allen Bdowney/10-2Ex.py
@@ -64,21 +64,74 @@ def is_anagram(word1, word2):
 d = ["add", "minus", "multi", "divide", "small", "large"]
 
 
-def has_duplicates(d_list):
-    bol=bool()
-    new_list = d_list
-    for i in range(0, len(d_list)):
-        for j in range(i+1, len(d_list)):    
+def has_duplicates(t):
+    """Returns True if any element appears more than once in a sequence.
+    t: list
+    returns: bool
+    """
+    # make a copy of t to avoid modifying the parameter
+    s = t[:]
+    s.sort()
+
+    # check for adjacent elements that are equal
+    for i in range(len(s)-1):
+        if s[i] == s[i+1]:
+            return True
+    return False
+
+
+def random_bdays(n):
+    """Returns a list of integers between 1 and 365, with length n.
+    n: int
+    returns: list of int
+    """
+    t = []
+    for i in range(n):
+        bday = random.randint(1, 365)
+        t.append(bday)
+    return t
+
+
+def count_matches(num_students, num_simulations):
+    """Generates a sample of birthdays and counts duplicates.
+    num_students: how many students in the group
+    num_samples: how many groups to simulate
+    returns: int
+    """
+    count = 0
+    for i in range(num_simulations):
+        t = random_bdays(num_students)
+        if has_duplicates(t):
+            count += 1
+    return count
+
+
+def main():
+    """Runs the birthday simulation and prints the number of matches."""
+    num_students = 23
+    num_simulations = 1000
+    count = count_matches(num_students, num_simulations)
+
+    print('After %d simulations' % num_simulations)
+    print('with %d students' % num_students)
+    print('there were %d simulations with at least one match' % count)
+
+
+# def has_duplicates(d_list):
+#     bol=bool()
+#     new_list = d_list
+#     for i in range(0, len(d_list)):
+#         for j in range(i+1, len(d_list)):    
            
-            if new_list[i] == d_list[j]:
-                print(j)
-                print(d_list[j])
-                bol = True
-                break
-    if bol:
-        return True
-    else:
-        return False
+#             if new_list[i] == d_list[j]:
+#                 print(j)
+#                 print(d_list[j])
+#                 bol = True
+#                 break
+#     if bol:
+#         return True
+#     else:
+#         return False
 
 
 # Ex 10-7
